cadeab2aab01933fc8c5a2df12a6629d1f964947
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-10-28 20:11:52 -0300

    chat de mensagens #2

---
+++ chat.py
@@ -0,0 +1,27 @@
+import os
+
+mensagens = []
+
+nome = input('Nome: ')
+
+while True:
+    
+    # limpando terminal
+    os.system('cls')
+
+    if len(mensagens) > 0:
+        for m in mensagens:
+            print(m['nome'], "-", m['texto'])
+    
+    print('_______________________')
+
+    # obtendo texto
+    texto = input('mensagens: ')
+    if texto == 'fim':
+        break
+    
+    # adicionando mensagem na lista
+    mensagens.append({
+        'nome': nome,
+        'texto': texto
+    })
